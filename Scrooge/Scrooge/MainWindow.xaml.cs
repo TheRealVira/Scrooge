41b1b7e4f482bdf564f8e6542fce2a93ff676499
Thomas Rührig <tom.ruehrig@aon.at>
2016-03-05 12:25:37 +0100

    Did some GUI Tests commit

--- Scrooge/Scrooge/MainWindow.xaml.cs
+++ Scrooge/Scrooge/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Scrooge
 {
@@ -23,6 +10,16 @@ namespace Scrooge
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GithubButton_Onclick(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/TheRealVira/Scrooge");
+        }
+
+        private void StackoverflowButton_Onclick(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://stackoverflow.com/l");
         }
     }
 }
