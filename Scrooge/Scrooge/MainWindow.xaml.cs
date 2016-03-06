6e59269bb07594bc44ade0c0edd1dd85a8bcdce3
Thomas Rührig <tom.ruehrig@aon.at>
2016-03-06 08:34:01 +0100

    Implemented fancy tabs commit

--- Scrooge/Scrooge/MainWindow.xaml.cs
+++ Scrooge/Scrooge/MainWindow.xaml.cs
@@ -1,25 +1,23 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Scrooge
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
-            InitializeComponent();
-        }
-
-        private void GithubButton_Onclick(object sender, RoutedEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/TheRealVira/Scrooge");
-        }
-
-        private void StackoverflowButton_Onclick(object sender, RoutedEventArgs e)
-        {
-            System.Diagnostics.Process.Start("http://stackoverflow.com/l");
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
