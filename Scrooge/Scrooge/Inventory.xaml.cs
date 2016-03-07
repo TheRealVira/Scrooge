fca59f67a29701d6e34ef2e8e6e011b1ce756b47
Thomas Rührig <tom.ruehrig@aon.at>
2016-03-07 10:21:45 +0100

    Added some testing logic commit

--- Scrooge/Scrooge/Inventory.xaml.cs
+++ Scrooge/Scrooge/Inventory.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Scrooge.Model;
 
 namespace Scrooge
 {
@@ -22,7 +23,7 @@ namespace Scrooge
     {
         public Inventory()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
         private void AddEntryBtn_OnClick(object sender, RoutedEventArgs e)
@@ -35,6 +36,16 @@ namespace Scrooge
 
         private void SaveBtn_OnClick(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.InventoryGrid.Items.Add(new InventoryViewModel()
+            { Access = 1f,AssetValue = 12,BalanceValue = 13f,CostValue = 15f,DateOfAcquisition = 1998,Derecognition = 0f,Duration = 10f,IsSelected = false,Months = 10,Name = "Computer",Percentage = 50});
+            this.InventoryGrid.Items.Add(new InventoryViewModel()
+            { Access = 1f, AssetValue = 12, BalanceValue = 13f, CostValue = 15f, DateOfAcquisition = 3000, Derecognition = 0f, Duration = 10f, IsSelected = false, Months = 10, Name = "Auto", Percentage = 50 });
+            this.InventoryGrid.Items.Add(new InventoryViewModel()
+            { Access = 1f, AssetValue = 12, BalanceValue = 13f, CostValue = 15f, DateOfAcquisition = 1900, Derecognition = 0f, Duration = 10f, IsSelected = false, Months = 10, Name = "Bildschirm", Percentage = 50 });
         }
     }
 }
