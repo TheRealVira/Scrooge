43efd558c0a1e2005b02c36f38725128ddcc5b6e
Stefan Reiter <Stefan.Reiter@studierende.htl-donaustadt.at>
2016-03-11 08:24:51 +0100

    I fixed the build!

--- Scrooge/Scrooge/Service/Implementations/Storage/MockupStorageService.cs
+++ Scrooge/Scrooge/Service/Implementations/Storage/MockupStorageService.cs
@@ -1,8 +1,19 @@
-﻿using Scrooge.Service.Definitions;
+﻿using System.Collections.Generic;
+using Scrooge.Model;
+using Scrooge.Service.Definitions;
 
 namespace Scrooge.Service.Implementations.Storage
 {
     public class MockupStorageService : IStorageService
     {
+        public IStorageService UpdateInventory(IEnumerable<InventoryViewModel> items)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<InventoryViewModel> RetrieveInventoryViewModels()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
