e94da60fd97f058ca2b01d9a7bdd54bbe3b4536f
Thomas Rührig <tom.ruehrig@aon.at>
2016-03-19 10:08:45 +0100

    Added missing file commit

---
+++ Scrooge/Scrooge/GroupedSaleOrPurchase.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Scrooge.Model;
+
+namespace Scrooge
+{
+    /// <summary>
+    /// Interaction logic for GroupedSaleOrPurchase.xaml
+    /// </summary>
+    public partial class GroupedSaleOrPurchase : UserControl
+    {
+        public GroupedSaleOrPurchase(GroupedPurchaseAndSalesViewModel saleOrPurchase)
+        {
+            InitializeComponent();
+
+            this.GroupeName.Text = saleOrPurchase.GroupName;
+
+            if (saleOrPurchase.PurchaseAndSales != null)
+            {
+                foreach (var purchaseAndSalese in saleOrPurchase.PurchaseAndSales)
+                {
+                    this.SaleOrPurchaseGrid.Items.Add(purchaseAndSalese);
+                }
+            }
+
+            this.MySum.DataContext = saleOrPurchase;
+        }
+
+        private void Grid_OnBeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
