d62fb7bfb6666a9894e07226fb32e4d642548c2a
PiMaker <Stefan.Reiter@studierende.htl-donaustadt.at>
2016-03-28 17:23:57 +0200

    Integrated TaxPayable

--- Scrooge/Scrooge/PurchaseAndSales.xaml.cs
+++ Scrooge/Scrooge/PurchaseAndSales.xaml.cs
@@ -6,23 +6,23 @@ using System.Windows;
 using System.Windows.Controls;
 using MaterialDesignThemes.Wpf;
 using Scrooge.Model;
+using Scrooge.Service;
+using Scrooge.Service.Definitions;
 
 namespace Scrooge
 {
-    using Scrooge.Service;
-    using Scrooge.Service.Definitions;
-    using Scrooge.Service.Implementations;
-
     /// <summary>
-    /// Interaction logic for PurchaseAndSales.xaml
+    ///     Interaction logic for PurchaseAndSales.xaml
     /// </summary>
     partial class PurchaseAndSales
     {
+        private readonly ObservableCollection<GroupedSaleOrPurchase> _data;
+
         public PurchaseAndSales()
         {
-            InitializeComponent();
+            this.InitializeComponent();
 
-            this._data =new ObservableCollection<GroupedSaleOrPurchase>();
+            this._data = new ObservableCollection<GroupedSaleOrPurchase>();
 
             this.GroupedItems.ItemsSource = this._data;
         }
@@ -31,16 +31,18 @@ namespace Scrooge
         {
             if (this._data.Count != 0) return;
             foreach (
-                GroupedPurchaseAndSalesViewModel viewModel in
+                var viewModel in
                     MainWindow.StorageService.RetrieveGroupedPurchaseAndSalesViewModels())
             {
                 this._data.Add(new GroupedSaleOrPurchase(viewModel));
             }
 
-            this.Plus.Text = this._data.Where(x => x.Data.Type == EntryType.Sale).Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) +
-                             "";
+            this.Plus.Text =
+                this._data.Where(x => x.Data.Type == EntryType.Sale).Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) +
+                "";
             this.Minus.Text =
-                this._data.Where(x => x.Data.Type == EntryType.Purchase).Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) + "";
+                this._data.Where(x => x.Data.Type == EntryType.Purchase)
+                    .Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) + "";
 
             this.UpdateSumAndTaxPayable();
         }
@@ -57,21 +59,22 @@ namespace Scrooge
             //show the dialog
             var result = await DialogHost.Show(view, "RootDialog", view.DialogHost_OnDialogClosing);
 
-            if (!(bool)result || !view.AllSet || view.Output == null) return;
+            if (!(bool) result || !view.AllSet || view.Output == null) return;
 
             //view.Output.ID = _data.Count != 0 ? _data.Max(x => x.Data.PurchaseAndSales.Max(y=>y.ID)) + 1 : 0; // may come back to us one day, for now it also works without
 
-            if (_data.Any(x => x.Data.GroupName == view.Output.GroupName && x.Data.Type == view.Output.Type))
+            if (this._data.Any(x => x.Data.GroupName == view.Output.GroupName && x.Data.Type == view.Output.Type))
             {
-                var current=_data.First(x => x.GroupeName.Text==view.Output.GroupName && x.Data.Type == view.Output.Type);
+                var current =
+                    this._data.First(x => x.GroupeName.Text == view.Output.GroupName && x.Data.Type == view.Output.Type);
                 current.Data.PurchaseAndSales.Add(view.Output.PurchaseAndSales[0]);
-                current.MySum.Text = current.Data.PurchaseAndSales.Sum(x => x.Value)+"";
-                UpdateCalculations(current.Data.Type);
+                current.MySum.Text = current.Data.PurchaseAndSales.Sum(x => x.Value) + "";
+                this.UpdateCalculations(current.Data.Type);
             }
             else
             {
-                _data.Add(new GroupedSaleOrPurchase(view.Output));
-                UpdateCalculations(view.Output.Type);
+                this._data.Add(new GroupedSaleOrPurchase(view.Output));
+                this.UpdateCalculations(view.Output.Type);
             }
         }
 
@@ -79,11 +82,15 @@ namespace Scrooge
         {
             if (whichSideToUpdate == EntryType.Sale)
             {
-                this.Plus.Text = this._data.Where(x => x.Data.Type == EntryType.Sale).Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) + "";
+                this.Plus.Text =
+                    this._data.Where(x => x.Data.Type == EntryType.Sale)
+                        .Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) + "";
             }
             else
             {
-                this.Minus.Text = this._data.Where(x => x.Data.Type == EntryType.Purchase).Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) + "";
+                this.Minus.Text =
+                    this._data.Where(x => x.Data.Type == EntryType.Purchase)
+                        .Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) + "";
             }
 
             this.UpdateSumAndTaxPayable();
@@ -91,16 +98,16 @@ namespace Scrooge
 
         private void DeleteEntryBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            for (int i = this._data.Count-1; i > -1; i--)
+            for (var i = this._data.Count - 1; i > -1; i--)
             {
-                var isChecked = ((GroupedSaleOrPurchase) this._data[i]).ImSelected.IsChecked;
-                if (isChecked != null && (bool)isChecked)
+                var isChecked = this._data[i].ImSelected.IsChecked;
+                if (isChecked != null && (bool) isChecked)
                 {
                     this._data.Remove(this._data[i]);
                     continue;
                 }
 
-                for (int i2 = this._data[i].Data.PurchaseAndSales.Count - 1; i2 > -1; i2--)
+                for (var i2 = this._data[i].Data.PurchaseAndSales.Count - 1; i2 > -1; i2--)
                 {
                     if (this._data[i].Data.PurchaseAndSales[i2].IsSelected)
                     {
@@ -109,31 +116,34 @@ namespace Scrooge
                 }
             }
 
-            this.Plus.Text = this._data.Where(x => x.Data.Type == EntryType.Sale).Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) +
-                             "";
+            this.Plus.Text =
+                this._data.Where(x => x.Data.Type == EntryType.Sale).Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) +
+                "";
             this.Minus.Text =
-                this._data.Where(x => x.Data.Type == EntryType.Purchase).Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) + "";
+                this._data.Where(x => x.Data.Type == EntryType.Purchase)
+                    .Sum(x => x.Data.PurchaseAndSales.Sum(y => y.Value)) + "";
             this.UpdateSumAndTaxPayable();
         }
 
         private void SaveBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            GroupedPurchaseAndSalesViewModel[] items = new GroupedPurchaseAndSalesViewModel[GroupedItems.Items.Count];
-            this.GroupedItems.Items.OfType<GroupedSaleOrPurchase>().Select(x=>x.Data).ToArray().CopyTo(items, 0);
+            var items = new GroupedPurchaseAndSalesViewModel[this.GroupedItems.Items.Count];
+            this.GroupedItems.Items.OfType<GroupedSaleOrPurchase>().Select(x => x.Data).ToArray().CopyTo(items, 0);
             MainWindow.StorageService.UpdateGroupedPurchaseAndSales(new List<GroupedPurchaseAndSalesViewModel>(items));
         }
 
         private void UpdateSumAndTaxPayable()
         {
             this.Summ.Text = (decimal.Parse(this.Plus.Text) - decimal.Parse(this.Minus.Text)) + "";
-            this.TaxPayable.Text = Singleton<ServiceController>.Instance.Get<ICalculationService>().CalculateTaxPayable(/* ToDo: pass the grouped purchases and sales view models */, DateTime.Now.Year);
+            this.TaxPayable.Text =
+                Singleton<ServiceController>.Instance.Get<ICalculationService>()
+                    .CalculateTaxPayable(this._data.Select(x => x.Data), DateTime.Now.Year)
+                    .ToString();
         }
 
         private void InventoryGrid_OnBeginningEdit(object sender, DataGridBeginningEditEventArgs e)
         {
             e.Cancel = true;
         }
-
-        private readonly ObservableCollection<GroupedSaleOrPurchase> _data;
     }
 }
