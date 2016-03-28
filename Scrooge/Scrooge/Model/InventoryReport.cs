48fbbb8b0e920b656125dfe91bada6c00b44262f
PiMaker <Stefan.Reiter@studierende.htl-donaustadt.at>
2016-03-28 18:52:49 +0200

    Fix?

--- Scrooge/Scrooge/Model/InventoryReport.cs
+++ Scrooge/Scrooge/Model/InventoryReport.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Scrooge.Model
+﻿namespace Scrooge.Model
 {
     using System.Collections.ObjectModel;
 
     public class InventoryReport
     {
-        public Collection<Inventory> Inventories { get; set; }
+        public Collection<InventoryViewModel> Inventories { get; set; }
 
         public int Year { get; private set; }
     }
