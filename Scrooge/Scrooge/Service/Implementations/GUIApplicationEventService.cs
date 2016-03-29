7b3120a3734dd3375d90ba910659d8740d350a14
PiMaker <Stefan.Reiter@studierende.htl-donaustadt.at>
2016-03-29 15:24:35 +0200

    Some visual fixed plus implementation of confirmation dialog when exiting with unsaved changes

--- Scrooge/Scrooge/Service/Implementations/GUIApplicationEventService.cs
+++ Scrooge/Scrooge/Service/Implementations/GUIApplicationEventService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Scrooge.Model.Internal;
 using Scrooge.Service.Definitions;
@@ -10,7 +9,8 @@ namespace Scrooge.Service.Implementations
 {
     public class GUIApplicationEventService : IApplicationEventService
     {
-        private readonly IList<IApplicationEventListener> listenerList = new List<IApplicationEventListener>(); 
+        private readonly IList<Func<Task<bool>>> closeRequestHandlers = new List<Func<Task<bool>>>();
+        private readonly IList<IApplicationEventListener> listenerList = new List<IApplicationEventListener>();
 
         public IApplicationEventService RegisterApplicationEventListener(IApplicationEventListener listener)
         {
@@ -35,6 +35,29 @@ namespace Scrooge.Service.Implementations
                 applicationEventListener.ApplicationClosing();
             }
 
+            return this;
+        }
+
+        public async Task<bool> ApplicationCloseRequest()
+        {
+            foreach (var closeRequestHandler in closeRequestHandlers)
+            {
+                if (!await closeRequestHandler())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///     Registers a close request handler. Important: The handler should return true when closing should proceed, false
+        ///     when it should be aborted. And remember: With great power comes great responsibility!
+        /// </summary>
+        public IApplicationEventService RegisterApplicationCloseRequestHandler(Func<Task<bool>> handler)
+        {
+            this.closeRequestHandlers.Add(handler);
             return this;
         }
     }
