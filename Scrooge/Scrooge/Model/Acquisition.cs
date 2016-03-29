c62568c5a36896727ff27254a32caff8185a1ac1
Thomas Rührig <tom.ruehrig@aon.at>
2016-03-29 18:36:24 +0200

    Fixed bug commit

--- Scrooge/Scrooge/Model/Acquisition.cs
+++ Scrooge/Scrooge/Model/Acquisition.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+using Scrooge.Annotations;
 
 namespace Scrooge.Model
 {
-    public class Acquisition
+    public class Acquisition:INotifyPropertyChanged
     {
         // Explicit constructor needed for serialization, do not remove!
         public Acquisition()
@@ -25,6 +28,14 @@ namespace Scrooge.Model
         {
             this.DateTime = dateTime;
             this.Value = value;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
